805433834658fdf5ff72decd110464f2c2585273
Pawandeep123 <Pawandeepgrewal1990@gmail.com>
2020-05-27 08:51:25 +0530

    Calling Homepage content from CMS

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,46 +1,58 @@
 import React from 'react'
-import { RightContent, Big } from '../UI/'
+import PropTypes from 'prop-types'
+
+import { StaticQuery, graphql } from 'gatsby'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap'
 import './styled.css'
 import Carousel from 'react-bootstrap/Carousel'
-class Testimonials extends React.Component {
-  render() {
-    return (
-      <div className="Testimonials-slider">
-        <Container>
-          <Row>
-            <Carousel>
-              <Carousel.Item>
-                <h2 className="h2">
-                  "The secret to their success is there ability to assemble
-                  talented teams of people that care. I think of Urbian as a
-                  critical member of my team, not some outsourced function."
-                </h2>
-                <span>- Keith Elliott, CEO of Met-S Care</span>
-              </Carousel.Item>
-              <Carousel.Item>
-                <h2 className="h2">
-                  "The secret to their success is there ability to assemble
-                  talented teams of people that care. I think of Urbian as a
-                  critical member of my team, not some outsourced function."
-                </h2>
-                <span>- Keith Elliott, CEO of Met-S Care</span>
-              </Carousel.Item>
-              <Carousel.Item>
-                <h2 className="h2">
-                  "The secret to their success is there ability to assemble
-                  talented teams of people that care. I think of Urbian as a
-                  critical member of my team, not some outsourced function."
-                </h2>
-                <span>- Keith Elliott, CEO of Met-S Care</span>
-              </Carousel.Item>
-            </Carousel>
-          </Row>
-        </Container>
-      </div>
-    )
-  }
+
+const TestimonialsData = ({ data }) => (
+  <div className="Testimonials-slider">
+    <Container>
+      <Row>
+        <Carousel>
+          {data.allContentfulTestimonials.nodes.map(Testimonials => (
+            <Carousel.Item>
+              <h2
+                className="h2"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    Testimonials.testimonialsText.childMarkdownRemark.html,
+                }}
+              />
+              <span>- {Testimonials.personName}</span>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Row>
+    </Container>
+  </div>
+)
+
+TestimonialsData.propTypes = {
+  data: PropTypes.object,
 }
 
-export default Testimonials
+export default function Testimonials(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query TestimonialsQuery {
+          allContentfulTestimonials {
+            nodes {
+              id
+              personName
+              testimonialsText {
+                childMarkdownRemark {
+                  html
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => <TestimonialsData data={data} {...props} />}
+    />
+  )
+}
